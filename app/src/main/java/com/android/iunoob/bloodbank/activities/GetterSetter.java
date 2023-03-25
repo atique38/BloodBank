95185171a60f81d0317c3853997e19955b39052e
Md. Atique Faisal <hojoborolosopnil@gmail.com>
2023-03-26 00:39:14 +0600

    a java file is created

---
+++ app/src/main/java/com/android/iunoob/bloodbank/activities/GetterSetter.java
@@ -0,0 +1,27 @@
+package com.android.iunoob.bloodbank.activities;
+
+public class GetterSetter {
+    String roll,bloodGroup;
+
+    public GetterSetter(String roll, String bloodGroup) {
+        this.roll = roll;
+        this.bloodGroup = bloodGroup;
+    }
+
+
+    public String getRoll() {
+        return roll;
+    }
+
+    public void setRoll(String roll) {
+        this.roll = roll;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+}
